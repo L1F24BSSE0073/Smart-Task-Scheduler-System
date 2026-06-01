75155a3a354935cbdf3789ebff4fa4fad7625b54
FIRST PUSH <L1F24BSSE0073@UCP.EDU.PK>
2026-06-01 18:40:01 +0500

    FSG

---
+++ MY PROJECT/src/Task.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+class Task {
+    int id;
+    String title;
+    String category;
+    int priority;
+    LocalDate deadline;
+    String status;
+
+    public Task(int id, String title, String category, int priority, LocalDate deadline) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.priority = priority;
+        this.deadline = deadline;
+        this.status = "Pending";
+    }
+}
